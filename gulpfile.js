eb44fbcd62c3efa31e1794c18be10ab03823d84a
AFlowOfCode <aflowofcode@gmail.com>
2020-05-16 21:35:37 -0500

    update gulp flow & metadata, auto-concat header

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,45 @@ var uglifyjs = require('uglify-es');
 var composer = require('gulp-uglify/composer');
 var pump = require('pump');
 var del = require('del');
+var concat = require('gulp-concat');
 // var sourcemaps = require('gulp-sourcemaps');
-
 var minify = composer(uglifyjs, console);
 
 gulp.task('compressJS', function (cb) {
-    // the same options as described above
-    var options = {};
-   
-    pump([
-        gulp.src('src/*.js'),
-        // sourcemaps.init(),
-        minify(options),
-        // sourcemaps.write('.'),
-        gulp.dest('dist')
-      ],
-      cb
-    );
-  });
+  // the same options as described above
+  var options = {};
+ 
+  return pump([
+      gulp.src('src/script.js'),
+      // sourcemaps.init(),
+      minify(options),
+      // sourcemaps.write('.'),
+      gulp.dest('dist'),
+    ],
+    cb
+  );
+});
 
-  gulp.task('clean:dist', function() {
-    return del.sync('dist');
-  });
+gulp.task('add-header', function() {
+  return pump([
+    gulp.src(['src/header.js', 'dist/script.js']),
+    concat('script.user.js'),
+    gulp.dest('dist')
+  ]);
+});
 
-  gulp.task('watch', function(){
-    gulp.watch('src/*.js', gulp.series('compressJS')); 
-  })
+gulp.task('del-script', function() {
+  del.sync('dist/script.js');
+  return Promise.resolve('');
+});
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+});
+
+gulp.task('watch', function(){
+  gulp.watch('src/*.js', gulp.series('compressJS')); 
+});
+
+// To compile just run $ gulp
+exports.default = gulp.series('compressJS', 'add-header', 'del-script');
---
+++ src/header.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         meaningful-forks
+// @homepage     https://github.com/aflowofcode/meaningful-forks
+// @namespace    http://tampermonkey.net/
+// @version      0.3
+// @description  Sort Github fork lists by the number of stars and commits ahead from the source repo.
+// @author       Kevin Li + community
+// @match        https://github.com/*/network/members
+// @grant        none
+// ==/UserScript==
