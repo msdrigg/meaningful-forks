7c3cf5bc97c8e8b289803093a7b512956829bb4b
AFlowOfCode <aflowofcode@gmail.com>
2020-05-17 04:59:38 -0500

    header version bump

--- src/header.js
+++ src/header.js
@@ -2,7 +2,7 @@
 // @name         meaningful-forks
 // @homepage     https://github.com/aflowofcode/meaningful-forks
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      1.1.0
 // @description  Sort Github fork lists by the number of stars and commits ahead from the source repo.
 // @author       Kevin Li + community
 // @match        https://github.com/*/network/members
